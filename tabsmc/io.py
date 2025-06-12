e26afa9d3d8a147b11e03226f99e3b5b4e2eb9af
João Loula <jloula@mit.edu>
2025-06-12 04:16:51 +0000

    fix: io auth token

--- tabsmc/io.py
+++ tabsmc/io.py
@@ -3,8 +3,7 @@ import numpy as np
 from jaxtyping import Float, Array
 from os import getenv
 
-ACCESS_TOKEN = "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"
-# getenv("HF_TOKEN")
+getenv("HF_TOKEN")
 
 
 def discretize_dataframe(df: pl.DataFrame, n_bins: int = 20):
